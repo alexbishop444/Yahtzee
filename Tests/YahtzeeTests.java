3a17740a7d18117e0ba8f0763aee9988a2afcf1c
Alex Bishop <alex.bishop@c02y47zmjg5j.myob.myobcorp.net>
2019-09-05 14:12:22 +1000

    added same numbers test

--- Tests/YahtzeeTests.java
+++ Tests/YahtzeeTests.java
@@ -5,17 +5,38 @@ import java.util.ArrayList;
 public class YahtzeeTests {
     GameMethods game = new GameMethods();
     @Test
-    public void ChanceTest() {
+    public void chanceTest() {
         int[] hand = {1,3,6,4,3};
         int expected = 17;
-        int actual = game.sumOfAllNumbers(hand);
+        int actual = game.chance(hand);
         Assert.assertEquals(expected,actual);
     }
     @Test
-    public void YahtzeeTest() {
+    public void yahtzeeTest() {
         int[] hand = {4,4,4,4,4};
         int expected = 50;
         int actual = game.yahtzee(hand);
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void addUpSameNumbersTestFour() {
+        int[] hand = {4,4,4,4,3};
+        int expected = 16;
+        int actual = game.addUpSameNumbers(hand,4);
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void addUpSameNumbersTestFive() {
+        int[] hand = {5,5,5,4,3};
+        int expected = 15;
+        int actual = game.addUpSameNumbers(hand,5);
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void addUpSameNumbersTestFive() {
+        int[] hand = {5,5,5,4,3};
+        int expected = 15;
+        int actual = game.addUpSameNumbers(hand,5);
         Assert.assertEquals(expected,actual);
     }
 }
