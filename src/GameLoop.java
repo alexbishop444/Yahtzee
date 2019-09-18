7fa85c0d3f514ca62e3acbfded47e1c4f74254d3
Alex Bishop <alex.bishop@myob.com>
2019-09-18 15:04:45 +1000

    loop working

--- src/GameLoop.java
+++ src/GameLoop.java
@@ -4,6 +4,7 @@ public class GameLoop {
     //all above are game categories
 
     boolean gameover;
+    boolean turn;
 
 
     Scanner scanner = new Scanner(System.in);
@@ -13,14 +14,23 @@ public class GameLoop {
     public void start() {
         System.out.println("Welcome to Yahtzee, Player one it is your turn");
         do {
-            System.out.println("These are your cards.");
-            System.out.println(playerOne.deck.toString());
-            options(playerOne);
-            choices(playerOne);
-            if(playerOne.checkTest()) {
-                gameover = true;
+            if(!turn) {
+                System.out.println("These are your cards.");
+                System.out.println(playerOne.deck.toString());
+                options(playerOne);
+                choices(playerOne);
+                System.out.println(turn);
             }
-
+            else if(turn) {
+                System.out.println("Player twos turn. These are your cards:");
+                System.out.println(playerTwo.deck.toString());
+                options(playerTwo);
+                choices(playerTwo);
+                if (playerTwo.checkTest()) {
+                    gameover = true;
+                }
+            }
+            System.out.println(turn);
         }while(!gameover);
     }
 
@@ -45,67 +55,97 @@ public class GameLoop {
     private void choices(Player player) {
         String choice = scanner.nextLine();
         if(choice.equals("1") && !player.chance) {
+            turn ^= true;
             player.score +=  score.chance(convertArray(player.deck));
             System.out.println(score.chance(convertArray(player.deck)));
+            player.setDeck(newRoll());
             player.chance = true;
         } else if (choice.equals("2") && !player.ones) {
+            turn ^= true;
             player.score +=  score.addUpSameNumbers((convertArray(player.deck)),1);
             System.out.println(score.addUpSameNumbers(convertArray(player.deck),1));
+            player.setDeck(newRoll());
             player.ones = true;
         } else if (choice.equals("3") && !player.twos) {
+            turn ^= true;
             player.score +=  score.addUpSameNumbers((convertArray(player.deck)),2);
             System.out.println(score.addUpSameNumbers(convertArray(player.deck),2));
+            player.setDeck(newRoll());
             player.twos = true;
         } else if (choice.equals("4") && !player.threes) {
+            turn ^= true;
             player.score += score.addUpSameNumbers((convertArray(player.deck)),3);
             System.out.println(score.addUpSameNumbers(convertArray(player.deck),3));
+            player.setDeck(newRoll());
             player.threes = true;
         } else if (choice.equals("5") && !player.fours) {
+            turn ^= true;
             player.score += score.addUpSameNumbers((convertArray(player.deck)),4);
             System.out.println(score.addUpSameNumbers(convertArray(player.deck),4));
+            player.setDeck(newRoll());
             player.fours = true;
         } else if (choice.equals("6") && !player.fives) {
+            turn ^= true;
             player.score += score.addUpSameNumbers(convertArray(player.deck),5);
             System.out.println(score.addUpSameNumbers(convertArray(player.deck),5));
+            player.setDeck(newRoll());
             player.fives = true;
         } else if (choice.equals("7") && !player.sixes) {
+            turn ^= true;
             player.score += score.addUpSameNumbers((convertArray(player.deck)),6);
             System.out.println(score.addUpSameNumbers(convertArray(player.deck),6));
+            player.setDeck(newRoll());
             player.sixes = true;
         } else if (choice.equals("8") && !player.pair) {
+            turn = true;
             player.score += score.pair(convertArray(player.deck));
             System.out.println(score.pair(convertArray(player.deck)));
+            player.setDeck(newRoll());
             player.pair = true;
         } else if (choice.equals("9") && !player.twoPairs) {
+            turn ^= true;
             player.score += score.twoPair(convertArray(player.deck));
             System.out.println(score.twoPair(convertArray(player.deck)));
+            player.setDeck(newRoll());
             player.twoPairs = true;
         } else if (choice.equals("10") && !player.threeOfAKind) {
+            turn ^= true;
             player.score += score.threes(convertArray(player.deck));
             System.out.println(score.threes(convertArray(player.deck)));
+            player.setDeck(newRoll());
             player.threeOfAKind = true;
         } else if (choice.equals("11") && !player.fourOfAKind) {
+            turn ^= true;
             player.score += score.foursCode(convertArray(player.deck));
             System.out.println(score.foursCode(convertArray(player.deck)));
+            player.setDeck(newRoll());
             player.fourOfAKind = true;
         } else if (choice.equals("12") && !player.smallStraight) {
+            turn ^= true;
             player.score += score.smallStraight(convertArray(player.deck));
             System.out.println(score.smallStraight(convertArray(player.deck)));
+            player.setDeck(newRoll());
             player.smallStraight = true;
         } else if (choice.equals("13") && !player.largeStraight) {
+            turn ^= true;
             player.score += score.largeStraight(convertArray(player.deck));
             System.out.println(score.largeStraight(convertArray(player.deck)));
+            player.setDeck(newRoll());
             player.largeStraight = true;
         } else if (choice.equals("14") && !player.fullHouse) {
+            turn ^= true;
             player.score += score.fullHouse(convertArray(player.deck));
             System.out.println(score.fullHouse(convertArray(player.deck)));
+            player.setDeck(newRoll());
             player.fullHouse = true;
         } else if (choice.equals("15") && !player.yahtzee) {
+            turn ^= true;
             player.score += score.yahtzee(convertArray(player.deck));
             System.out.println(score.yahtzee((convertArray(player.deck))));
+            player.setDeck(newRoll());
             player.yahtzee = true;
         } else {
-            System.out.println("You have already used this option");
+            System.out.println("Invalid Input try again");
         }
     }
 
