4db423ff91c0b83c4df617c322862621901ccb7a
Alex Bishop <alex.bishop@myob.com>
2019-09-20 11:44:10 +1000

    added hashmap

--- src/Player.java
+++ src/Player.java
@@ -1,31 +1,16 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Player {
     boolean roll1 = false;
     boolean roll2 = false;
     boolean roll3 = false;
-    boolean chance = false;
-    boolean yahtzee = false;
-
-
-    boolean ones = false;
-    boolean twos = false;
-    boolean threes = false;
-    boolean fours = false;
-    boolean fives = false;
-    boolean sixes = false;
-
-    boolean pair = false;
-    boolean twoPairs = false;
-    boolean threeOfAKind = false;
-    boolean fourOfAKind = false;
-    boolean smallStraight = false;
-    boolean largeStraight = false;
-    boolean fullHouse = false;
-
     int score = 0;
 
     ArrayList<Dice> deck;
+
+    ScoreCard scoreCardClass = new ScoreCard();
+    HashMap<String, Boolean> scoreCard = scoreCardClass.scoreCard;
 
     public void setDeck(ArrayList<Dice> deck) {
         this.deck = deck;
@@ -39,23 +24,10 @@ public class Player {
         this.deck = deck;
     }
 
-    public boolean check() {
-        if(chance && yahtzee && ones && twos && threes && fours && fives && sixes && pair && twoPairs && threeOfAKind && fourOfAKind && smallStraight && largeStraight && fullHouse ) {
-            return true;
-        }
-        return false;
-    }
-
     public void reset() {
         this.roll1 = true;
         this.roll2 = true;
         this.roll3 = true;
     }
 
-    public boolean checkTest() {
-        if (chance) {
-            return true;
-        }
-        return false;
-    }
 }
